e008be7c5b23f18a0e6e9d27674fbc239cade044
cy <390518881@qq.com>
2019-11-05 11:23:05 +0800

    提交

--- src/main/java/com/example/demo/model/UserModel.java
+++ src/main/java/com/example/demo/model/UserModel.java
@@ -6,13 +6,16 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
 
 /**
  * Description: PO类
  */
 @Data
 @ApiModel(value = "用户信息", description = "请求参数body")
-public class UserModel extends BaseProtocolIn {
+public class UserModel extends BaseProtocolIn implements Serializable {
+
+    private static final long serialVersionUID = 7034302540275683167L;
     @ApiModelProperty(value = "id")
     private String id;
     @ApiModelProperty(value = "用户名")
